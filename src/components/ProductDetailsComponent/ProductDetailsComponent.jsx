6e921e98e56eaeb459e8e2dd10c99c7d26689e65
UnCleFor <22520425@gm.uit.edu.vn>
2025-03-24 12:50:43 +0700

    Trang chi tiết sản phẩm (part 2)

--- src/components/ProductDetailsComponent/ProductDetailsComponent.jsx
+++ src/components/ProductDetailsComponent/ProductDetailsComponent.jsx
@@ -2,7 +2,7 @@ import { Col, Image, Row } from 'antd'
 import React from 'react'
 import imageProduct from '../../assets/images/z6436857502524_b8df322fa070c2dd15fc904c2ee1c100.jpg'
 import imageProductSmall from '../../assets/images/z6436857450475_595921696663d0fe0f381a7c4efb9de6.jpg'
-import { WrapperStyleColImage, WrapperStyleImageSmall, WrapperStyleNameProduct } from './style'
+import { DetailsCell, RowDetail, SizeBox, SizeProduct, TableProductDetails, TitleCell, WrapperStyleColImage, WrapperStyleImageSmall, WrapperStyleNameProduct, WrapperStylePriceProduct } from './style'
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons'
 
 const ProductDetailsComponent = () => {
@@ -59,10 +59,56 @@ const ProductDetailsComponent = () => {
                         <p><strong>Phân khúc:</strong> {product.category}</p>
                         <p><strong>Nhóm sản phẩm:</strong> {product.tags ? product.tags.join(", ") : "Không có thông tin"}</p>
                     </div>
+                    <div style={{ marginBottom: "20px", borderBottom: "1px solid #ddd", paddingBottom: "10px" }}>
+                        <WrapperStylePriceProduct>{new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(product.price)}</WrapperStylePriceProduct>
+                    </div>
+                    <div>
+                        <SizeProduct>
+                            <p><strong>Kích thước</strong></p>
+                            {product.sizes.map((size, index) => (
+                                <SizeBox key={index}>
+                                    {size}
+                                </SizeBox>
+                            ))}
+                            <SizeBox>
+                                order
+                            </SizeBox>
+                        </SizeProduct>
+                        <div>
+                            <p><strong>Màu sắc</strong></p>
+                        </div>
+                        <div>
+                            <Row>
+                                <Col span={12}>
+
+                                </Col>
+                                <Col span={12}></Col>
+                            </Row>
+                        </div>
+                        <div>
+                            ButtonComponent
+                        </div>
+                    </div>
                 </Col>
             </Row>
 
             <h2>Mô tả sản phẩm</h2>
+            <div className="border border-gray-300 rounded-md">
+                <TableProductDetails>
+                    <tbody>
+                        {product.details.map((item, index) => (
+                            <RowDetail key={index} even={index % 2 === 0}>
+                                <TitleCell>{item.label}</TitleCell>
+                                <DetailsCell>
+                                    {item.value.split(", ").map((line, i) => (
+                                        <div key={i}>{line}</div>
+                                    ))}
+                                </DetailsCell>
+                            </RowDetail>
+                        ))}
+                    </tbody>
+                </TableProductDetails>
+            </div>
         </div>
     )
 }
