eb10def23acca07b549e485e836a3344ec997d8a
ThanhNhaUIT <22520994@gm.uit.edu.vn>
2025-03-25 17:33:43 +0700

    Phân chia MVC

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 chạy server frontend thì cd vào frontend, backend cũng z, sau đó "npm start"
-nhớ tải các module cần thiết
+-> nhớ tải các module cần thiết
+
+-> tải extension Thunder Client để test api 
+-> đang để chuỗi kết nối của mongodb là của t
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mongoose  = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get('/',(req,res) => {
-    return res.send('Hello world hehe')
-});
+//
+app.use(bodyParser.json())
+// định tuyến
+routes(app);
+
+// Kết nối mongodb
+mongoose.connect(`${process.env.MONGO_DB}`)
+.then(() => {
+    console.log('Ket noi database thanh cong')
+})
+.catch((err) => {
+    console.log('Loi')
+})
+
+app.use(bodyParser.json())
 
 app.listen(port,() => {
     console.log('Server is running in port: ',+port)
