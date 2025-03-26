ed8fc2466cdcfc1b03c17822f1316517d6eeb5d5
ThanhNhaUIT <22520994@gm.uit.edu.vn>
2025-03-26 10:16:16 +0700

    API đăng ký User

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,9 +1,36 @@
 const UserService = require('../services/UserService')
+// validator cho email (npm install email-validator)
+var validator = require("email-validator");
+
 const createUser = async (req,res) => {
     try {
-        console.log(req.body)
-        const res = await UserService.createUser()
-        return res.status(200).json()
+        // lấy ra các thông tin cần thiết từ body của req đc gửi từ ui xuống để tạo user mới 
+        const {name, email, password, confirmPassword, phone } = req.body
+        
+        // check email có hợp lệ ko
+        const isCheckEmail = validator.validate(email);
+        
+        // nếu thiếu 1 trường thì báo lỗi
+        if ( !name || !email || !password || !confirmPassword || !phone ) {
+            return res.status(200).json({
+                status: 'Lỗi',
+                message: 'Cần điền đầy đủ thông tin'
+            })
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'Lỗi',
+                message: 'Email bị lỗi'
+            })
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({
+                status: 'Lỗi',
+                message: 'Mật khẩu nhập lại không trùng khớp'
+            })
+        }
+        
+        // thực hiện gọi dịch vụ tạo user mới
+        const ketqua = await UserService.createUser(req.body)
+        return res.status(200).json(ketqua)
     }
     catch(e){
         return res.status(404).json({
--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,7 +1,37 @@
-const createUser = () => {
-    return new Promise((resolve,reject) => {
+const User = require('../models/UserModel')
+const createUser = (newUser) => {
+    return new Promise( async(resolve,reject) => {
+        // lấy các trường ra từ input
+        const {name, email, password, confirmPassword, phone } = newUser
         try {
-            resolve({})
+            // biến kiểm tra coi mail có được xài chưa
+            const checkUser = await User.findOne({
+                email: email
+            })
+            // nếu khác null thì tức là đã có, == null tức là chưa có
+            if (checkUser !== null){
+                resolve({
+                    status: 'Oki',
+                    message: 'Email xài gòi á pà'
+                })
+            }
+            // tạo user mới
+            const createUser = await User.create({
+                name, 
+                email, 
+                password, 
+                confirmPassword, 
+                phone
+            })
+            // thông báo khi tạo user thành công
+            if (createUser) {
+                resolve({
+                    status:"Oki",
+                    message: "Tạo thành công",
+                    data: createUser
+                })
+            }
+            
         }
         catch(e) {
             reject(e)
