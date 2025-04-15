903e01dd6815187735e94451f7397c76521f2442
QuocThai1501 <22521318@gm.uit.edu.vn>
2025-04-15 16:13:22 +0700

    API tạo sản phẩm mới (part 2)

--- frontend/src/components/AdminProduct/AdminProduct.jsx
+++ frontend/src/components/AdminProduct/AdminProduct.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
-import { WrapperHeader } from './style'
+import React, { useEffect, useState } from 'react'
+import { WrapperHeader, WrapperUploadFile } from './style'
 import { Button, Modal, Form } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import TableComponent from '../TableComponent/TableComponent'
 import InputComponent from '../InputComponent/InputComponent'
 import { getBase64 } from '../../utils'
-import { WrapperUploadFile } from './style'
+import * as ProductService from '../../services/ProductService'
+import { useMutationHooks } from "../../hooks/useMutationHook";
+import Loading from '../LoadingComponent/Loading'
+import * as message from '../../components/Message/Message';
 
 const AdminProduct = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -17,14 +20,40 @@ const AdminProduct = () => {
     countInstock: '',
     description: ''
   })
-  // const handleOk = () => {
-  //   onFinish()
-  // }
+
+  const mutation = useMutationHooks(
+    (data) => {
+      const { name, image, type, price, countInstock: countInStock, description } = data
+      const res = ProductService.createProduct({ name, image, type, price, countInStock, description })
+      return res
+    }
+  )
+
+  const { data, isLoading, isSuccess, isError } = mutation
+
+  useEffect(() => {
+    if (isSuccess && data?.status === 'OK') {
+      message.success()
+      handleCancel()
+
+    } else if (isError) {
+      message.error()
+    }
+  }, [isSuccess])
+
   const handleCancel = () => {
     setIsModalOpen(false);
+    setStateProduct({
+      name: '',
+      image: '',
+      type: '',
+      price: '',
+      countInstock: '',
+      description: ''
+    })
   };
   const onFinish = () => {
-    console.log('finish', stateProduct)
+    mutation.mutate(stateProduct)
   }
   const handleOnchange = (e) => {
     setStateProduct({
@@ -53,79 +82,81 @@ const AdminProduct = () => {
         <TableComponent />
       </div>
       <Modal title="Tạo sản phẩm mới" open={isModalOpen} onCancel={handleCancel} okText=''>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Name"
-          name="Name"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <InputComponent value={stateProduct.name} onChange={handleOnchange} name="name"/>
-        </Form.Item>
+        <Loading isLoading={isLoading}>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Name"
+              name="Name"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <InputComponent value={stateProduct.name} onChange={handleOnchange} name="name" />
+            </Form.Item>
 
-        <Form.Item
-          label="Type"
-          name="Type"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <InputComponent value={stateProduct.type} onChange={handleOnchange} name="type"/>
-        </Form.Item>
+            <Form.Item
+              label="Type"
+              name="Type"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <InputComponent value={stateProduct.type} onChange={handleOnchange} name="type" />
+            </Form.Item>
 
-        <Form.Item
-          label="Price"
-          name="Price"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <InputComponent value={stateProduct.price} onChange={handleOnchange} name="price"/>
-        </Form.Item>
+            <Form.Item
+              label="Price"
+              name="Price"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <InputComponent value={stateProduct.price} onChange={handleOnchange} name="price" />
+            </Form.Item>
 
-        <Form.Item
-          label="Count inStock"
-          name="CountInStock"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <InputComponent value={stateProduct.countInstock} onChange={handleOnchange} name="countInstock"/>
-        </Form.Item>
-        
-        <Form.Item
-          label="Description"
-          name="Description"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <InputComponent value={stateProduct.description} onChange={handleOnchange} name="description"/>
-        </Form.Item>  
-              
-        <Form.Item
-          label="Image"
-          name="Image"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <WrapperUploadFile onChange={handleOnchangeAvatar} maxCount={1}>
-            <Button >Select File</Button>
-            {stateProduct?.image && (
-              <img src={stateProduct?.image} style={{
-                height: '60px',
-                width: '60px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-                marginLeft: '10px'
-              }}  alt="" />
-            )}
-          </WrapperUploadFile>
-        </Form.Item>  
-              
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item
+              label="Count inStock"
+              name="CountInStock"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <InputComponent value={stateProduct.countInstock} onChange={handleOnchange} name="countInstock" />
+            </Form.Item>
+
+            <Form.Item
+              label="Description"
+              name="Description"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <InputComponent value={stateProduct.description} onChange={handleOnchange} name="description" />
+            </Form.Item>
+
+            <Form.Item
+              label="Image"
+              name="Image"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <WrapperUploadFile onChange={handleOnchangeAvatar} maxCount={1}>
+                <Button >Select File</Button>
+                {stateProduct?.image && (
+                  <img src={stateProduct?.image} style={{
+                    height: '60px',
+                    width: '60px',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    marginLeft: '10px'
+                  }} alt="" />
+                )}
+              </WrapperUploadFile>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Loading>
       </Modal>
     </div>
   )
---
+++ frontend/src/services/ProductService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const getAllProduct = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
+    return res.data
+}
+
+// export const createProduct = async (data) => {
+//     const res = await axios.post(`${process.env.REACT_APP_API_URL}/product/create`, data)
+//     return res.data
+// }
+
+export const createProduct = async (data) => {
+    const token = localStorage.getItem('access_token') // hoặc từ Redux
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/product/create`,
+      data,
+      {
+        headers: {
+          token: `Bearer ${token}`
+        },
+        withCredentials: true
+      }
+    )
+    return res.data
+  }
+  
