225526ff15475ee0d36e59f839d52ad1557754dd
QuocThai1501 <22521318@gm.uit.edu.vn>
2025-05-06 10:18:20 +0700

    Fix lỗi deploy

--- backend/src/services/JwtService.js
+++ backend/src/services/JwtService.js
@@ -6,14 +6,14 @@ dotenv.config()
 // tạo access_token để mã hóa dữ liệu gửi đi
 const generalAccessToken = async (payload) => {
     //console.log('ASTPayload:', payload);
-    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN , { expiresIn: '365d' });
+    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN , { expiresIn: '1d' });
     return access_token;
 };
 
 // nếu access_token hết hạn thì dùng refresh token để làm mới lại access_token
 const generalRefreshToken = async (payload) => {
     //console.log('Payload:', payload);
-    const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN, { expiresIn: '3650d' });
+    const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN, { expiresIn: '365d' });
     return refresh_token
 };
 
